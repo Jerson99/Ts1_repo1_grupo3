dd7397be70f6aa36c11bc95ed05b668c44aa08dd
Juan Romero <muquirt@gmail.com>
2020-11-20 16:23:56 -0500

    Estructura v2

---
+++ lib.py
@@ -0,0 +1,11 @@
+def es_primo( num ):
+    return True
+
+def factorial( num ):
+    return 10
+
+def raiz_entera( num ):
+    return True
+
+def capicua( num ):
+    return True
---
+++ principal.py
@@ -0,0 +1,3 @@
+import lib
+
+print( lib.es_primo( 12 ) )
