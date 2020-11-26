d232d0da562a32274860dfec1963e7297b6048ce
josh18151 <74795522+josh18151@users.noreply.github.com>
2020-11-26 13:01:54 -0500

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,3 +1,15 @@
 import lib
 
-print( lib.es_primo( 12 ) )
+def f(n):
+    a=1
+    if n<0:
+        return 0
+    elif n==0:
+        return 0
+    else:
+        for i in range(2,n+1):
+            a=a*i
+        return a
+
+.
+
