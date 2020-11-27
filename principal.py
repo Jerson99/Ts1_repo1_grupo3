bddb77b2cca2c86618bad1ac73cc5896f802ab72
josh18151 <74795522+josh18151@users.noreply.github.com>
2020-11-27 15:58:54 -0500

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,19 +1,8 @@
 import lib
 
-print( lib.es_primo( 12 ) )
-
-n=int(input("ingrese un numero: "))
-def evaluar_primo(n):
-    contador=0
-    resultado=True
-    for i in range(1,n+1):
-        if (n%i==0):
-            contador+=1
-        if (contador>2):
-            resultado=False
-            break
-    return resultado
-if (evaluar_primo(n)==True):
-    print("el numero es primo")
-else :
-    print("el numero no es primo")
+factorial = 1
+f = int(input("Ingrese el numero:   "))
+for x in range (1,f+1):
+    factorial = factorial * x
+ 
+print (" El factorial del número",f," ", "es",factorial)
